ee86f37364777e0098b035049737912c9b9c9bd0
Leticia Gomes <leticia.m.gomes10@aluno.senai.br>
2025-05-19 09:41:12 -0300

    feat: finaliza resolução da questão 02

---
+++ scripts/questao-02.js
@@ -0,0 +1,24 @@
+const numeros = [12, 5, 8, 21, 16, 7, 30, 45, 13, 27];
+let numerosPares = 0;
+let numerosImpares = 0;
+let maioresQue20 = 0;
+
+// Varredura do array
+for (let i = 0; i < numeros.length; i++) {
+// Condicional para numeros pares
+    if (numeros[i] % 2 == 0) {
+     numerosPares++; 
+    }else {
+      numerosImpares++; 
+    } 
+
+    // Condicional para maiores que 20
+ if (numeros[i] > 20) {
+    maioresQue20++;
+ }
+}
+
+console.log(`A quantidade de numeros pares é: ${numerosPares}`);
+console.log(`A quantidade de numeros impares é: ${numerosImpares}`);
+console.log(`A quantidade de numeros maiores que 20 é: ${maioresQue20}`);
+
